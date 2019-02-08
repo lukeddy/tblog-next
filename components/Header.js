aa33162461efc889df1a7c4ebc594bd9f5134365
tzq668766 <tzq618766@gmail.com>
2019-02-08 14:41:15 +0800

    项目初始化

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from 'next/link'
+
+const linkStyle = {
+    marginRight: 15,
+    fontSize:'16px',
+    color:'#f00'
+}
+
+const Header = () => (
+    <div>
+        <Link href="/">
+            <a style={linkStyle}>Home</a>
+        </Link>
+        <Link href="/about">
+            <a style={linkStyle}>About</a>
+        </Link>
+    </div>
+)
+
+export default Header
