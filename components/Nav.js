7a53dc09b0b15769cab53f5e3151b1e4af6016a2
tzq668766 <tzq618766@gmail.com>
2019-02-26 15:36:33 +0800

    更新导航组件

--- components/Nav.js
+++ components/Nav.js
@@ -1,37 +1,41 @@
 import React from "react";
 import Link from 'next/link'
 
-const Nav = () => (
-    <div className="navbar navbar-inverse navbar-fixed-top">
-        <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-            </button>
-            <Link href="/">
-                <a className="navbar-brand hidden-sm">
-                    <span className="tblog-leaf-logo">&nbsp;</span>
-                </a>
-            </Link>
-            <form className="navbar-form pull-right" action="/search">
-                <div className="form-group hidden-xs">
-                    <div className="input-group">
-                        <div className="input-group-addon"><i className="glyphicon glyphicon-search"></i></div>
-                        <input type="text" name="keywords" className="form-control" placeholder=""/>
-                    </div>
+class Nav extends React.Component{
+    render(){
+        return(
+            <div className="navbar navbar-inverse navbar-fixed-top">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <Link href="/">
+                        <a className="navbar-brand hidden-sm">
+                            <span className="tblog-leaf-logo">&nbsp;</span>
+                        </a>
+                    </Link>
+                    <form className="navbar-form pull-right" action="/search">
+                        <div className="form-group hidden-xs">
+                            <div className="input-group">
+                                <div className="input-group-addon"><i className="glyphicon glyphicon-search"></i></div>
+                                <input type="text" name="keywords" className="form-control" placeholder=""/>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-        <div className="navbar-collapse collapse pull-right">
-            <ul className="nav navbar-nav">
-                <li><Link href="/"><a>首页</a></Link></li>
-                <li><Link href="/about"><a>关于</a></Link></li>
-                <li><Link href="/register"><a>注册</a></Link></li>
-                <li><Link href="/login"><a>登陆</a></Link></li>
-            </ul>
-        </div>
-    </div>
-)
+                <div className="navbar-collapse collapse pull-right">
+                    <ul className="nav navbar-nav">
+                        <li><Link href="/"><a>首页</a></Link></li>
+                        <li><Link href="/about"><a>关于</a></Link></li>
+                        <li><Link href="/register"><a>注册</a></Link></li>
+                        <li><Link href="/login"><a>登陆</a></Link></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Nav
