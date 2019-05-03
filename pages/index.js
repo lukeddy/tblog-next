b1730b85e198d54fd114cc0123247852aba0580d
tzq668766 <tzq618766@gmail.com>
2019-05-03 21:10:52 +0800

    首页内容分页实现

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Router from 'next/router';
 import Advertise from "../components/Advertise";
 import Pagination from "../components/common/Pagination";
 import Alert from '../components/common/Alert';
@@ -8,83 +9,56 @@ import {BarLoader} from 'react-spinners';
 import axios from "axios";
 import {connect} from 'react-redux';
 import Nav from '../components/Nav'
+import {fetchHomeData} from '../store/actions/postActions';
 import {logout} from '../store/actions/authActions';
 
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state={
-            currentFilter:{
-                pageNO:1,
-                tab:"all"
-            },
-            loading:false,
-            alertData:null,
+            currentTab:"all",
         }
 
         //一定要写这个binding，不然在调用分页接口时会报goToPage is not a function
-        this.loadData=this.loadData.bind(this);
         this.goToPage=this.goToPage.bind(this);
     }
 
-    static async getInitialProps() {
+    static async getInitialProps({store,req,query}) {
+        const {pageSize=3,pageNO=1,tab="all"}=query
         // console.log('初始化')
+        console.log('query',query)
+        console.log('store',store)
+        const currentFilter={pageNO:pageNO, tab: tab}
         try{
-            const params={pageNO: 1,tab:"all"}
+            const params={pageSize:pageSize,pageNO: pageNO,tab:tab}
             const response =await axios.post("/home",params)
             const {status,msg,data}=response.data
             //console.log('data aaa:',status,data)
             if(status){
-                return {catList:data.catList,pager:data.pager,currentTab:data.indexVo.tab,alertData:null}
+                return {catList:data.catList,pager:data.pager,currentTab:data.indexVo.tab,alertData:null,currentFilter}
             }else{
-                return {alertData:{status:false,msg:msg}}
+                return {alertData:{status:false,msg:msg},currentFilter}
             }
         }catch(error){
-            return {alertData:{status:false,msg:error.message}}
+            return {alertData:{status:false,msg:error.message},currentFilter}
         }
-    }
-
-   async loadData(pageNo,tab){
-        //console.log("loading data: "+pageNo);
-       try{
-           this.setState({loading:true});
-           const response =await axios.post("/home",{pageNO:pageNo,tab:tab})
-           const {status,msg,data}=response.data
-           if(status){
-               this.setState({pager:response.data.data.pager});
-               this.setState({catList:response.data.data.catList});
-               this.setState({currentFilter:response.data.data.indexVo});
-           }else{
-               this.setState({alertData:response.data});
-           }
-           this.setState({loading:false});
-       }catch(error){
-           console.log(error);
-           this.setState({loading:false});
-           this.setState({alertData:{status:false,msg:"获取帖子数据失败"}});
-       }
     }
 
     goToPage(pageNo){
         //console.log("go to page:"+pageNo);
-        this.setState({
-            currentFilter: { ...this.state.currentFilter, pageNo: pageNo }
-        },this.loadData(pageNo,this.state.currentFilter.tab));
+        console.log('current tab:',this.state.currentTab)
+        Router.push(`/?pageNO=${pageNo}&tab=${this.state.currentTab}`)
     }
 
 
     goToTab=(tab)=>{
-        console.log('goto tab:',tab)
-        this.setState({
-            currentFilter: { ...this.state.currentFilter, tab: tab }
-        });
-
-        this.loadData(1,tab);
+        // console.log('goto tab:',tab)
+        this.setState({currentTab: tab});
+        Router.push(`/?pageNO=1&tab=${tab}`)
     }
 
     render() {
-        const {currentFilter, alertData,loading}=this.state
-        const {pager,catList,isAuthenticated}=this.props
+        const {pager,catList,currentFilter,alertData,isAuthenticated}=this.props
 
         return (
             <div className="container main">
@@ -92,18 +66,18 @@ class Index extends Component {
                 {/*{isAuthenticated && <h1>成功登陆</h1>}*/}
                 {alertData && <Alert alertData={alertData}/>}
                 <div className="col-md-9">
-                    <BarLoader loading={loading} widthUnit={'px'} heightUnit={'px'} width={823} height={6} color={'#fa0000'}/>
-                    {!loading &&<div className="panel">
+                    {/*<BarLoader loading={loading} widthUnit={'px'} heightUnit={'px'} width={823} height={6} color={'#fa0000'}/>*/}
+                    <div className="panel">
                         <div className="header">
                             {catList &&<Menu catList={catList} currentTab={currentFilter.tab} goToTab={this.goToTab}/>}
                         </div>
                         <div className="inner no-padding">
                             {pager&&pager.content.length===0 && <div className='row text-center'>没有数据</div>}
                             {pager&&pager.content.length>0 && <PostList postList={pager.content}></PostList>}
-                            {pager&&pager.totalPages>0 &&<Pagination totalPages={pager.totalPages} currentPage={pager.number+1} jumpPage={this.goToPage}/> }
+                            {pager&&pager.totalPages>0 &&<Pagination totalPages={pager.totalPages} currentPage={pager.number+1} jumpPage={this.goToPage.bind(this)}/> }
                         </div>
                     </div>
-                    }
+
                 </div>
                 <div className="col-md-3">
                     <Advertise/>
@@ -119,4 +93,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps,{logout})(Index)
+export default connect(mapStateToProps,{fetchHomeData,logout})(Index)
